2c3130be99255f8999a0d21b60dfc45bf1c05abe
Adrian Bjugård <adrian@qng.se>
2015-02-27 15:39:19 +0100

    upgraded server!

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 import socketserver
+import argparse
 
 class DockerTransferServer(socketserver.StreamRequestHandler):
     def handle(self):
         self.data = self.rfile.readline().strip()
         print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        self.wfile.write(self.data.upper())
+        print(self.data.decode('utf-8'))
+        string = input("Svar: ")
+        self.wfile.write(("rekoil: "+string).encode('utf-8'))
 
 if __name__ == "__main__":
-    HOST, PORT = "0.0.0.0", 9999
-    server = socketserver.TCPServer((HOST, PORT), DockerTransferServer)
+    parser = argparse.ArgumentParser(description='Server for docker-transfer service')
+    parser.add_argument('-p', '--port', dest='port', nargs=1, default=1208)
+    parser.add_argument('-H', '--host', dest='host', nargs='?', default='localhost')
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true')
+    parser.set_defaults(verbose=True)
+    args = parser.parse_args()
+
+    server = socketserver.TCPServer((args.host, args.port), DockerTransferServer)
     server.serve_forever()
