1321aa6490a7e1ef965f2f4baf660c5786d8cfb3
Johan Lindskogen <johan.lindskogen@gmail.com>
2015-02-27 15:46:12 +0100

    change to ?

--- compress.py
+++ compress.py
@@ -4,17 +4,13 @@ import subprocess
 
 def main():
     parser = argparse.ArgumentParser(description='Compress directory with Dockerfile')
-    parser.add_argument('-o', '--output', nargs=1, default='output')
+    parser.add_argument('-o', '--output', nargs='?', default='output')
     parser.add_argument('input', nargs='?')
     args = parser.parse_args()
     dockerfile = args.input + '/Dockerfile'
     if os.path.exists(dockerfile):
         print("File exists: {0}".format(dockerfile))
-        if isinstance(args.output, list):
-            output = args.output[0]
-        else:
-            output = args.output
-        command = ['tar', 'czf', output + '.tar.gz', args.input]
+        command = ['tar', 'czf', args.output + '.tar.gz', args.input]
         # print(' '.join(command))
         subprocess.Popen(command)
     else:
