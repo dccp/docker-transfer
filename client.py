2c34eb65d8c95162e1ba01896f0aa15d2ea9d699
Johan Lindskogen <johan.lindskogen@gmail.com>
2015-03-03 16:36:58 +0100

    everything working

--- client.py
+++ client.py
@@ -1,27 +1,37 @@
 import argparse
 import socket
-import sys
 
+READ_SIZE = 1024
 
 def main():
     parser = argparse.ArgumentParser(description='Send work to Zeppelin network')
     parser.add_argument('-H', '--host', nargs='?', default='localhost')
     parser.add_argument('-p', '--port', nargs='?', default=1208)
-    parser.add_argument('message', nargs='+')
+    parser.add_argument('filename', nargs='?')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.set_defaults(verbose=True)
     args = parser.parse_args()
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    data = ' '.join(args.message)
+    sock = socket.socket()
+    sock.connect((args.host, args.port))
+    if args.verbose:
+        print("Connected on {}:{}".format(args.host, args.port))
 
     try:
-        sock.connect((args.host, args.port))
-        sock.sendall(bytes(data + "\n", "utf-8"))
-        received = str(sock.recv(1024), "utf-8")
+        if args.verbose:
+            print("Reading file \"{}\"".format(args.filename))
+        handle = open(args.filename, 'rb')
+        kilobyte = handle.read(READ_SIZE)
+        while (kilobyte):
+            sock.send(kilobyte)
+            if args.verbose:
+                print("Sent {} bytes".format(READ_SIZE))
+            kilobyte = handle.read(READ_SIZE)
     finally:
+        handle.close()
         sock.close()
-
-    print("Sent:     {}".format(data))
-    print("Received: {}".format(received))
+        if args.verbose:
+            print("Socket closed")
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,21 +1,51 @@
-import socketserver
 import argparse
+import socket
 
-class DockerTransferServer(socketserver.StreamRequestHandler):
-    def handle(self):
-        self.data = self.rfile.readline().strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data.decode('utf-8'))
-        string = input("Svar: ")
-        self.wfile.write(("rekoil: "+string).encode('utf-8'))
+READ_SIZE = 1024
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description='Server for docker-transfer service')
-    parser.add_argument('-p', '--port', dest='port', nargs=1, default=1208)
-    parser.add_argument('-H', '--host', dest='host', nargs='?', default='localhost')
-    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true')
+    parser.add_argument('-H', '--host', nargs='?', default='localhost')
+    parser.add_argument('-p', '--port', nargs='?', default=1208)
+    parser.add_argument('filename', nargs='?', default='recieved')
+    parser.add_argument('-v', '--verbose', action='store_true')
     parser.set_defaults(verbose=True)
     args = parser.parse_args()
 
-    server = socketserver.TCPServer((args.host, args.port), DockerTransferServer)
-    server.serve_forever()
+    server = socket.socket()
+    server.bind((args.host, args.port))
+    server.listen(10)
+    if args.verbose:
+        print("Listening on {}:{}".format(args.host, args.port))
+    i = 0
+
+
+    try:
+        while True:
+            i += 1
+            sc, address = server.accept()
+            if args.verbose:
+                print("Client connected: {}".format(address))
+            line = sc.recv(READ_SIZE)
+            handle = open(args.filename + str(i), 'wb')
+            while line:
+                print(line)
+                if args.verbose:
+                    print("Recieved {} bytes".format(READ_SIZE))
+                handle.write(line)
+                line = sc.recv(READ_SIZE)
+
+            handle.close()
+            sc.close()
+            if args.verbose:
+                print("Client disconnected: {}".format(address))
+
+        server.close()
+    finally:
+        handle.close()
+        sc.close()
+        server.close()
+
+
+if __name__ == "__main__":
+    main()
