fd003b8af773246b9ab8a0bf4f48c2d69fa608db
Adrian Bjugård <adrian@qng.se>
2015-03-06 16:13:37 +0100

    bind to all addresses

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ READ_SIZE = 1024
 
 def main():
     parser = argparse.ArgumentParser(description='Server for docker-transfer service')
-    parser.add_argument('-H', '--host', nargs='?', default='localhost')
+    parser.add_argument('-H', '--host', nargs='?', default='0.0.0.0')
     parser.add_argument('-p', '--port', nargs='?', default=1208)
     parser.add_argument('filename', nargs='?', default='recieved')
     parser.add_argument('-v', '--verbose', action='store_true')
@@ -28,10 +28,11 @@ def main():
                 print("Client connected: {}".format(address))
             line = sc.recv(READ_SIZE)
             handle = open(args.filename + str(i), 'wb')
+            j = 0
             while line:
-                print(line)
+                j += 1
                 if args.verbose:
-                    print("Recieved {} bytes".format(READ_SIZE))
+                    print("Recieved {} bytes\r".format(READ_SIZE * j), end="")
                 handle.write(line)
                 line = sc.recv(READ_SIZE)
 
